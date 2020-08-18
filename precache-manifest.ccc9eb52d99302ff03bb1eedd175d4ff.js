21117089723727942e0348fa9efb76daf0041843
Ali Sadoughi <asadoughi1990@gmail.com>
2020-08-18 12:22:50 +0430

    Updates

---
+++ precache-manifest.ccc9eb52d99302ff03bb1eedd175d4ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e675b5f33c9a8943fd6e51441cc2ff",
+    "url": "/Lightech/index.html"
+  },
+  {
+    "revision": "9dc0a514da1aa235ba82",
+    "url": "/Lightech/static/css/main.0ce549a4.chunk.css"
+  },
+  {
+    "revision": "6a6a1648b38264ad48dd",
+    "url": "/Lightech/static/js/2.28bfd264.chunk.js"
+  },
+  {
+    "revision": "5776768c082169f6074c75f93f8b9356",
+    "url": "/Lightech/static/js/2.28bfd264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc0a514da1aa235ba82",
+    "url": "/Lightech/static/js/main.5d7baa2a.chunk.js"
+  },
+  {
+    "revision": "4babe8046b95220cbebd",
+    "url": "/Lightech/static/js/runtime-main.0dca645b.js"
+  },
+  {
+    "revision": "95477d3d8fa04c7b2a073a88ae967a71",
+    "url": "/Lightech/static/media/img5.95477d3d.jpg"
+  },
+  {
+    "revision": "3c1e04901e63bef6c26cbe06c067ca2c",
+    "url": "/Lightech/static/media/ionicons.3c1e0490.eot"
+  },
+  {
+    "revision": "43d5c718339efeb1177aff722d72669d",
+    "url": "/Lightech/static/media/ionicons.43d5c718.woff2"
+  },
+  {
+    "revision": "91433571f154776a1d46c2a287de05d7",
+    "url": "/Lightech/static/media/ionicons.91433571.svg"
+  },
+  {
+    "revision": "a21b9e1aad96623ef91bca7ecea7b169",
+    "url": "/Lightech/static/media/ionicons.a21b9e1a.woff"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/Lightech/static/media/ionicons.b2e0fc82.ttf"
+  }
+]);
